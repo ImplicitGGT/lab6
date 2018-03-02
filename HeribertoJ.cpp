12824eb7dc12fd6697279b54319bd1f2e714f78a
Student <student@lab-315-027.cs.csubak.edu>
2018-03-02 14:11:58 -0800

    add my individual file

---
+++ HeribertoJ.cpp
@@ -0,0 +1,31 @@
+//
+//
+//
+//
+//program: asteroids.cpp
+//author:  Heriberto Jimenez
+//date:    2014 - 2018
+//mod spring 2015: added constructors
+//mod spring 2018: X11 wrapper class
+//This program is a game starting point for a 3350 project.
+//
+
+#include "fonts.h"
+
+
+void showName()
+{
+	Rect r;
+	//
+	r.bot = 20;
+	r.left = 10;
+	r.center = 0;
+	ggprint8b(&r, 16, 0x00ff0000, "Heriberto Jimenez");
+
+}
+
+
+
+
+
+
